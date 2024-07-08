6f3a873d29a0d91d77fbfbc13863d9e450402cac
madorTill <mador.till@gmail.com>
2024-07-08 10:18:52 +0300

    added image plugin to vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// added plugin to deal with the svg problem
+import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +16,9 @@ export default defineConfig({
 
   // added to deal with the svg problem
   rollupOptions: {
+    plugins: [
+      image()
+    ],
     // Ensure to include the file extensions in the external option
     external: ['.svg'],
   },
